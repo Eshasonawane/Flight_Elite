1c29d75522fc2d5e16f4e408802dab5bb8f52b32
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-07-13 16:31:51 +0530

    push the code

--- src/main/java/com/mmcoe/entity/Flight.java
+++ src/main/java/com/mmcoe/entity/Flight.java
@@ -1,5 +1,46 @@
 package com.mmcoe.entity;
 
 public class Flight {
-
+	int code;
+	String carrier;
+	String source;
+	String destination;
+	@Override
+	public String toString() {
+		return "Flight [code=" + code + ", carrier=" + carrier + ", source=" + source + ", destination=" + destination
+				+ "]";
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getCarrier() {
+		return carrier;
+	}
+	public void setCarrier(String carrier) {
+		this.carrier = carrier;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public Flight() {
+	}
+	public Flight(int code, String carrier, String source, String destination) {
+		this.code = code;
+		this.carrier = carrier;
+		this.source = source;
+		this.destination = destination;
+	}
+	
 }
